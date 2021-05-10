1709e0d20a1b1f554080633dd11ad1c871490d33
张三疯 <659966631@qq.com>
2021-05-10 17:30:35 +0800

    自带的App

---
+++ zxymultilingual/src/main/java/com/zxy/zxymultilingual/Applications.java
@@ -0,0 +1,73 @@
+package com.zxy.zxymultilingual;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Method;
+
+public class Applications {
+
+
+    @NonNull
+
+    public static Application context() {
+
+        return CURRENT;
+
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+
+    private static final Application CURRENT;
+
+
+    static {
+
+        try {
+
+            Object activityThread = getActivityThread();
+
+            Object app = activityThread.getClass().getMethod("getApplication")
+
+                    .invoke(activityThread);
+
+            CURRENT = (Application) app;
+
+        } catch (Throwable e) {
+
+            throw new IllegalStateException("Can not access Application context by magic code, boom!", e);
+
+        }
+
+    }
+
+
+    private static Object getActivityThread() {
+
+        Object activityThread = null;
+
+        try {
+
+            @SuppressLint("PrivateApi") Method method = Class.forName("android.app.ActivityThread")
+
+                    .getMethod("currentActivityThread");
+
+            method.setAccessible(true);
+
+            activityThread = method.invoke(null);
+
+        } catch (final Exception e) {
+
+            Log.w("zxy", e);
+
+        }
+
+        return activityThread;
+
+    }
+
+}
